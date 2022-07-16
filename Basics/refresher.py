56a7ddbd2f880cd801f2b13694854e2fd11f15cd
stevensecreti <stevensecreti4@gmail.com>
2022-07-16 14:59:26 -0400

    Completed Coursework

---
+++ Basics/refresher.py
@@ -0,0 +1,22 @@
+#LISTS: Variables that stores comma separated values
+mylist = ["a", "b", "c"]
+#Get elements by index
+#mylist[0] == "a"
+
+#Dictionaries: Store key value pairs -- like HashMap
+d = {
+    "Python": "Programming",
+    "English": "Natural",
+    "C++": "Programming",
+    "Java": "Programming",
+}
+#keys = d.keys()
+#values = d.values()
+#items = d.items() (Keys and values)
+
+#Functions:
+def my_function(something):
+    return something
+
+def information(word):
+    return "Word: " + str(word) + "Length: " + str(len(word))
---
+++ README.md
@@ -0,0 +1,4 @@
+### Overview
+ - This codebase exists as a byproduct of my first attempt to learn NLP with no prior experience.
+ - This repository and all of the code in it was created by myself while following the course found on UDEMY listed below.
+    - https://www.udemy.com/course/natural-language-processing/
